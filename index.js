8696c416300d2318e16b59a82472555150bbed6e
Aman Raj <razzaman9834@gmail.com>
2024-01-06 17:43:10 +0530

    testkeys

--- index.js
+++ index.js
@@ -118,7 +118,7 @@ app.get('/testsecret', async (req, res) => {
       });
   } catch (error) {
       console.error('Error:', error.message);
-      res.status(500).json({ error: 'Failed to retrieve secret from Azure Key Vault' });
+      res.status(200).json({ error: 'Failed to retrieve secret from Azure Key Vault' });
   }
 });
 
