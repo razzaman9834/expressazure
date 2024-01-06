4217d638a05776e4dc61fe2dfc0b4a66504f48e0
Aman Raj <razzaman9834@gmail.com>
2024-01-06 10:06:35 +0530

    monogo

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 //mongodb://myazuretesting:27UqwL7Z3fWWv1duwKCh4j0YDiA36ZALQKj1OU1VXFqL7H27xs4Un8OHpBzgeqgj6GpVGSrKH6HmACDb18N3GA%3D%3D@myazuretesting.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@myazuretesting@
-
+const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 
@@ -11,6 +11,26 @@ app.use(express.json());
 
 const port = process.env.PORT || 3000;
 
+const mongoURI = 'mongodb://myazuretesting:27UqwL7Z3fWWv1duwKCh4j0YDiA36ZALQKj1OU1VXFqL7H27xs4Un8OHpBzgeqgj6GpVGSrKH6HmACDb18N3GA%3D%3D@myazuretesting.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@myazuretesting@'; // Replace with your MongoDB URI
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Define a simple mongoose model and schema
+const Task = mongoose.model('Task', {
+  name: String,
+  description: String,
+});
+
+// Express route to fetch tasks from MongoDB
+app.get('/tasks', async (req, res) => {
+  try {
+    // Query all tasks from the database
+    const tasks = await Task.find();
+    res.json(tasks);
+  } catch (error) {
+    console.error('Error fetching tasks:', error.message);
+    res.status(500).send('Internal Server Error');
+  }
+});
 
 
 app.get('/test', function (req, res) {
