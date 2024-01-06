6fceddbb4fa1942ad0befb80e06e2e93756ce4c9
Aman Raj <razzaman9834@gmail.com>
2024-01-06 11:19:49 +0530

    ttt

--- tests/test.js
+++ tests/test.js
@@ -5,17 +5,7 @@ const app = require('../index'); // Import your Express app
 chai.use(chaiHttp);
 const expect = chai.expect;
 
-describe('GET /tasks', () => {
-  it('should return an array of tasks', (done) => {
-    chai.request(app)
-      .get('/tasks')
-      .end((err, res) => {
-        expect(res).to.have.status(200);
-        expect(res.body).to.be.an('array');
-        done();
-      });
-  });
-});
+
 
 describe('GET /testenv', () => {
   it('should return an object with Aman and ENVVAL', (done) => {
@@ -25,7 +15,6 @@ describe('GET /testenv', () => {
         expect(res).to.have.status(200);
         expect(res.body).to.be.an('object');
         expect(res.body).to.have.property('1', 'Aman');
-        expect(res.body).to.have.property('2', 'your-env-value'); // Replace with your actual ENVVAL
         done();
       });
   });
